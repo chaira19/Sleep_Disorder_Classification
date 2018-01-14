bfc30a5232c377adb811078bd4e59ebd1147b258
Chirayu-sopho <chirayuasati19@gmail.com>
2018-01-14 11:55:07 +0530

    function for joining features and labels

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
+## library to play with physionet datasets
 import wfdb
 
-#data = {}
+## Function to extract features from data file and labels from annotaion file and link them.
+## File name inputs are without formats.
+## Function appends lists containg features and labels(as last feature) to the input list.
+def annotation_features(Data_Filename, Annotation_Filename, data = []):
+
+	record = wfdb.rdsamp('Sleep_MIT_Dataset/slp01a')                                                
+	signals, fields = wfdb.srdsamp('Sleep_MIT_Dataset/slp01a', sampfrom=1, sampto = record.siglen)
+	annotations = [i.strip().split() for i in open("Dataset_Modified_Form/" + Annotation_Filename + ".txt").readlines()]
+
+	for item in annotations:
+		value = []
+
+		#if item[1][0] is 'D' :
+		#	continue
+
+		if int(item[2]) < int(record.siglen):
+
+			value.append(signals[int(item[2])])
+			value.append(item[7])
+			data.append(value)
+
+	#return data
+
+data = []
+
+annotation_features('slp01a', "annotations1", data)
+
+for d in data:
+	print d
 
 # function to load and add data from text file
 #def append_from_text(previous_data, newfile_path)
 
-annotations = [i.strip().split() for i in open("Dataset_Modified_Form/annotations1.txt").readlines()]
 
 ### original_data = [i.strip().split() for i in open("Dataset_Modified_Form/slp01b.txt").readlines()]
 
 #print annotations
 #print original_data
 
-record1 = wfdb.rdsamp('Sleep_MIT_Dataset/slp01a')
-
-record2 = wfdb.rdsamp('Sleep_MIT_Dataset/slp02b')
-
-signals1, fields1 = wfdb.srdsamp('Sleep_MIT_Dataset/slp01a', sampfrom=1, sampto = record1.siglen)
-
-signals2, fields2 = wfdb.srdsamp('Sleep_MIT_Dataset/slp02b', sampfrom=1, sampto = record2.siglen)
-
-newdata = {}
-
-k = 0
 
 
-for item in annotations:
 
-	value = []
 
-	#if item[1][0] is 'D' :
-	#	continue
-
-	if int(item[2]) < 1800000:
-		
-		#value.append(original_data[int(item[2])+2])
-		value.append(signals1[int(item[2])+2])
-		#value.append(signals1)
-		value.append(item[7])
-		newdata[k] = value
-		k = k + 1
 		
 	#print item
 	#print "\n"
@@ -47,7 +51,7 @@ for item in annotations:
 
 
 
-annotations1 = [i.strip().split() for i in open("Dataset_Modified_Form/annotations5.txt").readlines()]
+'''annotations1 = [i.strip().split() for i in open("Dataset_Modified_Form/annotations5.txt").readlines()]
 
 #original_data1 = [i.strip().split() for i in open("Dataset_Modified_Form/slp14.txt").readlines()]
 
@@ -64,7 +68,5 @@ for item in annotations1:
 		value.append(item[7])
 		newdata[k] = value
 		k = k + 1
+'''
 
-for item in newdata:
-	print item, newdata[item]
-	print "\n"
